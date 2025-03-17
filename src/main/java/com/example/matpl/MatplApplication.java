119daa9be31a0d27fa912e441667a4391614d627
Baskervir <persona903@gmail.com>
2025-03-17 22:32:48 +0900

    start

---
+++ src/main/java/com/example/matpl/MatplApplication.java
@@ -0,0 +1,13 @@
+package com.example.matpl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatplApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatplApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/matpl/MatplApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.matpl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatplApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
