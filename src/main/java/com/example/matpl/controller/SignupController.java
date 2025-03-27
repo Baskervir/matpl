fe0d6b43fffbea915d959811c1e02998b50f4caf
Baskervir <persona903@gmail.com>
2025-03-28 01:34:00 +0900

    로그인 기능 구현 시작

--- src/main/java/com/example/matpl/controller/SignupController.java
+++ src/main/java/com/example/matpl/controller/SignupController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/users")
 @RequiredArgsConstructor
-public class UserController {
+public class SignupController {
     private final SignupUseCase signup;
     private final SignupVerifyUserUseCase signupVerifyUserUseCase;
 
